945732ece059a3dcb062615050d6c6553c64118c
Nils Stolpe <nils.stolpe@nearform.com>
2023-11-07 10:07:08 -0500

    feat: added endpoints to get user info and to logout

---
+++ packages/server/src/plugins/logout.js
@@ -0,0 +1,16 @@
+import fp from 'fastify-plugin'
+
+async function logout(fastify) {
+  fastify.get('/logout', async (request, reply) => {
+    if (request.session.user) {
+      const message = `${request.session.user.userName} successfully logged out`
+      request.session.destroy(() => {
+        reply.send({
+          message
+        })
+      })
+    }
+  })
+}
+
+export default fp(logout)
